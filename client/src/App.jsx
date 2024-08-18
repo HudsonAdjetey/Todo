e9361ea198552dc4b58c8eeeb6a0bca2ba8a0658
HudsonAdjetey <adjetey545@gmail.com>
2024-08-18 16:51:24 -0700

    added get data function

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,20 @@
-import React from "react";
+import React, { useEffect } from "react";
 import ListHeader from "./components/ListHeader";
 const App = () => {
+  const getData = async () => {
+    try {
+      const result = await fetch(`http://localhost:5294/todos/${userEmail}`);
+      const data = await result.json();
+      console.log(data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    getData();
+  }, []);
+
   return (
     <div className="app">
       <ListHeader listName={"Todo-App"} />
