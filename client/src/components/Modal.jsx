519dd5c46f3adbb73353dc5e3790a73862e5edaa
HudsonAdjetey <adjetey545@gmail.com>
2024-08-19 04:09:05 -0700

    modal

--- client/src/components/Modal.jsx
+++ client/src/components/Modal.jsx
@@ -1,7 +1,43 @@
 import React from "react";
 
 const Modal = () => {
-  return <div>Modal</div>;
+  const mode = "create";
+  const handleChange = () => {
+    console.log("Changing");
+  };
+  return (
+    <div className="overlay">
+      <div className="modal">
+        <div className="form-title-container">
+          <h3>Let's {mode} your task</h3>
+          <button>X</button>
+        </div>
+        <form>
+          <input
+            type="text"
+            required
+            maxLength={30}
+            placeholder="Your task goes here"
+            name="title"
+            value={""}
+            onChange={handleChange}
+          />
+          <br />
+          <input
+            type="range"
+            required
+            min="0"
+            max="100"
+            step="1"
+            name="progress"
+            value={""}
+            onChange={handleChange}
+          />
+          <input type="submit" className={mode} />
+        </form>
+      </div>
+    </div>
+  );
 };
 
 export default Modal;
