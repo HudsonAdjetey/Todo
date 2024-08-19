96d86eab896e509d34d5950267c12e88edaa0f8b
HudsonAdjetey <adjetey545@gmail.com>
2024-08-19 11:08:34 -0700

    Readme

--- index.js
+++
@@ -1,9 +0,0 @@
-const task = {
-    title: 'Task 1',
-    description: 'Description for Task 1',
-    status: 'pending',
-    dueDate: new Date('2022-01-31'),
-    priority: 'high',
-    assignedTo: 'John Doe'
-}
-const newMap  = new Array(task)
