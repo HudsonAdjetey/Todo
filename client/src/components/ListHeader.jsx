e9d3ca1958fb0fc339a4ab7ca87af17a041bab03
HudsonAdjetey <adjetey545@gmail.com>
2024-08-19 04:03:39 -0700

    header button

--- client/src/components/ListHeader.jsx
+++ client/src/components/ListHeader.jsx
@@ -8,7 +8,7 @@ const ListHeader = ({ listName }) => {
     <div className="list-header">
       <h1>{listName}</h1>
       <div className="button-container">
-        <button className="create">Add New</button>
+        <button className="create-button">Add New</button>
         <button className="signout" onClick={signOut}>
           Sign Out
         </button>
