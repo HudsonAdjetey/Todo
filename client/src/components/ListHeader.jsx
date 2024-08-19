82e2426e1ea86cc46c28cb8c54b0635083a3703b
HudsonAdjetey <adjetey545@gmail.com>
2024-08-19 04:12:52 -0700

    modal style

--- client/src/components/ListHeader.jsx
+++ client/src/components/ListHeader.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import Modal from "./Modal";
 
 const ListHeader = ({ listName }) => {
   const signOut = () => {
@@ -13,6 +14,7 @@ const ListHeader = ({ listName }) => {
           Sign Out
         </button>
       </div>
+      <Modal />
     </div>
   );
 };
